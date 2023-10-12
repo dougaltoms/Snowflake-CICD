9bef4437a0a5cd48485810b2fdbc4abf372aa6c9
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2023-10-12 14:07:17 +0100

    Update validate.py

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -1,24 +1,27 @@
 def main():
 
     import subprocess
+    import sys
 
-    with open("files.txt", "r") as file:
+    with open('files.txt', 'r') as file:
         sql_files = file.read().splitlines()
 
     for script in sql_files:
-        
-        if '.sql' in script:
-            print("="*70)
-            print(f"Validating {script}")
+
+        if script.endswith('.sql'):
+
+            print('='*70)
+            print(f'Validating {script}')
+
             result = subprocess.run(['sqlfluff', 'lint', script, '-d', 'snowflake'] , capture_output=True, text=True)
-            
+    
             if result.returncode == 1:
-                print(f"{result.stdout}")
+                print("Validation FAILED")
                 sys.exit(1)
 
             else:
-                print(result.stdout)
-                print("="*70)   
+                print(f'{result.stdout}')
+                print('='*70)
 
 if __name__ == "__main__":
     main()
