36465308c6708e95d952955d8087a5f6f5e1794c
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2023-10-12 14:18:55 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,14 @@
 create or replace table test (col1 int, col2 varchar);
 
 select col1 from test;
+
+create or replace function addone(i int)
+returns int
+language python
+runtime_version = '3.8'
+handler = 'addone_py'
+as
+$$
+def addone_py(i):
+  return i+1
+$$;
