763cb328c198c46a58b8944aa1178748f29d27d1
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2023-10-13 11:45:57 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,6 +2,10 @@ create or replace table test (col1 int, col2 varchar);
 
 select col1 from test;
 
+create or replace procedure central_default_000_cod.prc_test();
+
+create or replace task central_default_000_cod.tsk_test();
+
 create or replace function addone(i int)
 returns int
 language python
