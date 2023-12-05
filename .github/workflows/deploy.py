fc1ad639ae40cd7beccc23dfd28ed47c6418b47c
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2023-12-05 10:29:14 +0000

    Update deploy.py

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -1,10 +1,21 @@
 def main():
 
-    import time
-    print('Deploying...')
-    time.sleep(3)
+    from snowflake.snowpark import Session
+    import pandas as pd
+    
+    snowflake_connection_parameters = {
+                            "account": "nh13284.west-europe.azure"
+                            , "user": "DOUGALTOMS" 
+                            , "password": "Password123"
+                            , "role": "ACCOUNTADMIN"
+                            , "warehouse": "COMPUTE_WH"
+                            }
+    session = Session.builder.configs(snowflake_connection_parameters).create()
 
-    return print('Successfully deployed')
+    print('Starting Deployment')
+    df = session.sql('''SELECT * FROM SNOWFLAKE.ACCOUNT_USAGE.LOGIN_HISTORY;''').to_pandas()
+    df.head()
+    return print('Deployment Successful')
 
 if __name__ == "__main__":
     main()
